002def7b6fc756ee5143fe5abf0795138c040e98
Tymur Mamedov1 <Tymur_Mamedov1@epam.com>
2019-09-02 14:32:09 +0300

    add new function for ger command from message and add info about song duration

--- utils/Utils.js
+++ utils/Utils.js
@@ -8,7 +8,7 @@ function getConcatenateValueFromMap(map) {
     let result = 'Here is a list of tracks\n';
     for (let value of map.values())
         for (let song of value.songs)
-            result += `${song.title}\n`;
+            result += `${song.title} (${song.duration})\n`;
 
     return result;
 }
@@ -16,10 +16,16 @@ function getConcatenateValueFromMap(map) {
 function getMessageOfSearchedResult(searchedResult) {
     let message = 'Choose a song by commenting a number between 1 and 5 or exit\n';
     for (let i = 0; i < searchedResult.length; i++) {
-        message += `Song ${i + 1} - ${searchedResult[i].title}\n`;
+        message += `Song ${i + 1} - ${searchedResult[i].title} (${searchedResult[i].duration})\n`;
     }
 
     return message;
+}
+
+function getCommandFromMessage(message) {
+    if (message.content.indexOf(" ") === -1)
+        return message.content;
+    return message.content.slice(0, message.content.indexOf(" "));
 }
 
 function getMessageContentAfterCommand(message) {
@@ -30,5 +36,6 @@ module.exports = {
     isEmpty: isEmpty,
     getConcatenateValueFromMap : getConcatenateValueFromMap,
     getMessageOfSearchedResult : getMessageOfSearchedResult,
+    getCommandFromMessage: getCommandFromMessage,
     getMessageContentAfterCommand : getMessageContentAfterCommand
 };
